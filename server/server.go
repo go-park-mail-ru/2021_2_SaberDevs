8cf1e228d3ca83632861ce604690a77ca83bd814
Yura <yura11011@ya.ru>
2021-09-26 18:50:00 +0300

    fixing articles

--- server/server.go
+++ server/server.go
@@ -348,8 +348,3 @@ func Run() {
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
-{
-	"status":200,
-	"body":
-	{"from":"0","to":"2","chunk":[{"id":"1","previewUrl":"static/img/computer.png","title":"7 Skills of Highly Effective Programmers","text":"Our team was inspired by the seven skills of highly effective","authorUrl":"#","authorName":"Григорий","authorAvatar":"static/img/photo-elon-musk.jpg","commentsUrl":"#","comments":97,"likes":1001,"tags":["IT-News","Study"]},{"id":"2","previewUrl":"static/img/computer.png","title":"7 Skills of Highly Effective Programmers","text":"Our team was inspired by the seven skills of highly effective","authorUrl":"#","authorName":"Григорий","authorAvatar":"static/img/photo-elon-musk.jpg","commentsUrl":"#","comments":97,"likes":1002,"tags":["IT-News","Study"]}]}
-	}
