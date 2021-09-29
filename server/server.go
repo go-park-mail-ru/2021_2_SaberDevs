9b2dfd92d9857b011f7390f77c82ea67ff661195
Yura <yura11011@ya.ru>
2021-09-29 13:50:50 +0300

    registration ch

--- server/server.go
+++ server/server.go
@@ -1,12 +1,13 @@
 package server
 
 import (
-	"fmt"
-	"github.com/labstack/echo/v4/middleware"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
+
+	"github.com/labstack/echo/v4/middleware"
 
 	uuid "github.com/satori/go.uuid"
 
@@ -15,9 +16,12 @@ import (
 
 type (
 	User struct {
-		ID       uint   `json:"id"`
+		Login    string `json:"login"`
+		Name     string `json:"name"`
+		Surname  string `json:"surname"`
 		Email    string `json:"email"`
 		Password string `json:"password"`
+		Score    uint   `json:"score"`
 	}
 
 	RequestUser struct {
@@ -27,7 +31,7 @@ type (
 	}
 
 	LoginBody struct {
-		ID      uint   `json:"id"`
+		Login   string `json:"login"`
 		Surname string `json:"surname"`
 		Name    string `json:"name"`
 		Email   string `json:"email"`
@@ -46,24 +50,32 @@ type (
 	}
 
 	SignUpBody struct {
-		ID   uint   `json:"id"`
-		Name string `json:"name"`
+		Login   string `json:"login"`
+		Surname string `json:"surname"`
+		Name    string `json:"name"`
+		Email   string `json:"email"`
+		Score   int    `json:"score"`
 	}
 
 	RequestSignup struct {
+		Login    string `json:"login"`
 		Email    string `json:"email"`
 		Password string `json:"password"`
+		Name     string `json:"name"`
+		Surname  string `json:"surname"`
 	}
 
 	GoodSignupResponse struct {
 		Status uint       `json:"status"`
-		SBody  SignUpBody `json:"body"`
+		SBody  SignUpBody `json:"data"`
+		Msg    string     `json:"msg"`
 	}
 
 	//Представление записи
 	NewsRecord struct {
 		Id           string   `json:"id"`
 		PreviewUrl   string   `json:"previewUrl"`
+		Tags         []string `json:"tags"`
 		Title        string   `json:"title"`
 		Text         string   `json:"text"`
 		AuthorUrl    string   `json:"authorUrl"`
@@ -72,56 +84,75 @@ type (
 		CommentsUrl  string   `json:"commentsUrl"`
 		Comments     uint     `json:"comments"`
 		Likes        uint     `json:"likes"`
-		Tags         []string `json:"tags"`
 	}
 
 	//Тело ответа на API-call /getfeed
-	Chunk struct {
-		From      string       `json:"from"`
-		To        string       `json:"to"`
-		ChunkData []NewsRecord `json:"chunk"`
-	}
 
 	RequestChunk struct {
-		From int `json:"from"`
-		To   int `json:"to"`
+		idLastLoaded string
+		login        string
 	}
 
 	ChunkResponse struct {
-		Status          uint  `json:"status"`
-		NewsRecordChunk Chunk `json:"body"`
+		Status    uint         `json:"status"`
+		ChunkData []NewsRecord `json:"data"`
 	}
 
 	ErrorBody struct {
 		Status   uint   `json:"status"`
-		ErrorMsg string `json:"error"`
+		ErrorMsg string `json:"msg"`
 	}
 
 	MyHandler struct {
-		sessions map[string]uint
+		sessions map[string]string
 		sMu      sync.RWMutex
 		users    map[string]User
 		uMu      sync.RWMutex
 	}
 )
 
-var endOfFeed = NewsRecord{"endOfFeedMarkerID", "static/img/endOfFeed.png",
-	"А всё, а раньше надо было", "", "#", "Tester-ender",
-	"static/img/loader-1-HorizontalBalls.gif", "#", 0, 0, []string{"Bottom"},
-}
+var feedSize int = 5
 
 func NewMyHandler() MyHandler {
 	return MyHandler{
-		sessions: make(map[string]uint, 10),
+		sessions: make(map[string]string, 10),
 		users: map[string]User{
-			"mollenTEST1":   {1, "mollenTEST1", "mollenTEST1"},
-			"dar@exp.ru":      {2, "dar@exp.ru", "123"},
-			"viphania@exp.ru": {3, "viphania@exp.ru", "123"},
+			"mollenTEST1":     {"mollenTEST1", "mollenTEST1", "mollenTEST1", "mollenTEST1", "mollenTEST1", 123456},
+			"dar@exp.ru":      {"dar@exp.ru", "dar@exp.ru", "dar@exp.ru", "dar@exp.ru", "123", 13553},
+			"viphania@exp.ru": {"viphania@exp.ru", "viphania@exp.ru", "viphania@exp.ru", "viphania@exp.ru", "123", 120},
+			"DenisTest": {"DenisTest", "DenisTest1", "DenisTest1", "DenisTest1@exp.ru", "DenisTest1", 120},
 		},
 	}
 }
 
 func (api *MyHandler) Login(c echo.Context) error {
+	// проверяем активные сессии
+	cooke, err := c.Cookie("session")
+	if err == nil {
+		api.sMu.RLock()
+		login, ok := api.sessions[cooke.Value]
+		api.sMu.RUnlock()
+		if ok {
+			api.uMu.RLock()
+			user, _ := api.users[login]
+			api.uMu.RUnlock()
+
+			b := LoginBody{
+				Login:   user.Login,
+				Name:    user.Email,
+				Surname: user.Email,
+				Email:   user.Email,
+				Score:   12345678, //rand.Int(),
+			}
+			response := GoodLoginResponse{
+				Status: http.StatusOK,
+				Data:   b,
+				Msg:    "OK",
+			}
+
+		return c.JSON(http.StatusOK, response)
+	}
+	}
 	// достаем данные из запроса
 	requestUser := new(RequestUser)
 	if err := c.Bind(requestUser); err != nil {
@@ -159,21 +190,21 @@ func (api *MyHandler) Login(c echo.Context) error {
 	cookie.Name = "session"
 	cookie.Value = uuid.NewV4().String()
 	cookie.HttpOnly = true
-	cookie.Expires = time.Now().Add(10 * time.Second)
+	cookie.Expires = time.Now().Add(10 * time.Hour)
 	c.SetCookie(cookie)
 	//
 	// добавляем пользователя в активные сессии
 	api.sMu.Lock()
-	api.sessions[cookie.Value] = user.ID
+	api.sessions[cookie.Value] = user.Login
 	api.sMu.Unlock()
 
 	// формируем ответ
 	b := LoginBody{
-		ID:      user.ID,
+		Login:   user.Login,
 		Name:    user.Email,
 		Surname: user.Email,
 		Email:   user.Email,
-		Score:   rand.Int(),
+		Score:   12345678, //rand.Int(),
 	}
 	response := GoodLoginResponse{
 		Status: http.StatusOK,
@@ -222,9 +253,9 @@ func (api *MyHandler) Register(c echo.Context) error {
 		}
 	}
 	// логика регистрации,  добавляем юзера в мапу
-	user := User{uint(len(api.users)), newUser.Email, newUser.Email}
+	user := User{newUser.Login, newUser.Name, newUser.Surname, newUser.Email, newUser.Password, 12345}
 	api.uMu.Lock()
-	api.users[newUser.Email] = user
+	api.users[newUser.Login] = user
 	api.uMu.Unlock()
 
 	// ставим куку на сутки
@@ -236,14 +267,21 @@ func (api *MyHandler) Register(c echo.Context) error {
 	//
 	// добавляем пользователя в активные сессии
 	api.sMu.Lock()
-	api.sessions[cookie.Value] = user.ID
+	api.sessions[cookie.Value] = user.Login
 	api.sMu.Unlock()
 
 	// формируем ответ
-	s := SignUpBody{user.ID, user.Email}
+	s := SignUpBody{
+		Login:   user.Login,
+		Name:    user.Name,
+		Surname: user.Surname,
+		Email:   user.Email,
+		Score:   12345678, //rand.Int(),
+	}
 	response := GoodSignupResponse{
 		Status: http.StatusOK,
 		SBody:  s,
+		Msg:    "OK",
 	}
 
 	return c.JSON(http.StatusOK, response)
@@ -272,7 +310,7 @@ func (api *MyHandler) Logout(c echo.Context) error {
 
 func (api *MyHandler) Root(c echo.Context) error {
 	b := LoginBody{
-		ID:      1,
+		Login:   "user.Email",
 		Name:    "user.Email",
 		Surname: "user.Email",
 		Email:   "user.Email",
@@ -287,61 +325,93 @@ func (api *MyHandler) Root(c echo.Context) error {
 }
 
 func (api *MyHandler) Getfeed(c echo.Context) error {
-	// достаем данные из запроса
-	requestChunk := new(RequestChunk)
-	if err := c.Bind(requestChunk); err != nil {
+	rec := c.QueryParam("idLastLoaded")
+	// TODO костыль!!!!
+	if rec == "" {
+		rec = "0"
+	}
+
+	from, err := strconv.Atoi(rec)
+	if err != nil {
 		errorJson := ErrorBody{
 			Status:   http.StatusNotFound,
-			ErrorMsg: "Wrong request",
+			ErrorMsg: "Not a feed Number",
 		}
 		c.Logger().Printf("Error: %s", err.Error())
 		return c.JSON(http.StatusNotFound, errorJson)
 	}
-
-	f := requestChunk.From
-	t := requestChunk.To
-
+	to := from + 4
+	var ChunkData []NewsRecord
 	//Возвращаем записи
-	chunk := Chunk{}
-	chunk.From = fmt.Sprint(requestChunk.From)
-	chunk.To = fmt.Sprint(requestChunk.To)
-	if f >= 0 && t >= 0 && t >= f && t < len(testData) {
+	if from >= 0 && to < len(testData) {
 		api.sMu.RLock()
-		chunk.ChunkData = testData[f:t]
+		ChunkData = testData[from:to]
 		api.sMu.RUnlock()
 	} else {
 		api.sMu.RLock()
-		chunk.ChunkData = []NewsRecord{endOfFeed}
+		start := 0
+		if len(testData) > 6 {
+			start = len(testData) - 6
+		}
+		ChunkData = testData[start : len(testData)-1]
 		api.sMu.RUnlock()
 	}
 	// формируем ответ
 	response := ChunkResponse{
 		http.StatusOK,
-		chunk,
+		ChunkData,
 	}
 	return c.JSON(http.StatusOK, response)
 }
 
 var testData = [...]NewsRecord{
-	{"1", "static/img/computer.png", "7 Skills of Highly Effective Programmers",
+	{"1", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
 		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
-		"#", 97, 1001, []string{"IT-News", "Study"},
+		"#", 97, 1001,
 	},
-	{"2", "static/img/computer.png", "7 Skills of Highly Effective Programmers",
+	{"2", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
 		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
-		"#", 97, 1002, []string{"IT-News", "Study"},
+		"#", 97, 1002,
 	},
-	{"3", "static/img/computer.png", "7 Skills of Highly Effective Programmers",
+	{"3", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
 		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
-		"#", 97, 1003, []string{"IT-News", "Study"},
+		"#", 97, 1003,
 	},
-	{"4", "static/img/computer.png", "7 Skills of Highly Effective Programmers",
+	{"4", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
 		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
-		"#", 97, 1004, []string{"IT-News", "Study"},
+		"#", 97, 1004,
 	},
-	{"5", "static/img/computer.png", "7 Skills of Highly Effective Programmers",
+	{"5", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
 		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
-		"#", 97, 1005, []string{"IT-News", "Study"},
+		"#", 97, 1005,
+	},
+	{"6", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
+		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
+		"#", 97, 1006,
+	},
+	{"7", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
+		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
+		"#", 97, 1007,
+	},
+	{"8", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
+		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
+		"#", 97, 14,
+	},
+	{"9", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
+		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
+		"#", 97, 1008,
+	},
+	{"10", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
+		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
+		"#", 97, 1009,
+	},
+	{"11", "static/img/computer.png", []string{"IT-News", "Study"}, "7 Skills of Highly Effective Programmers",
+		"Our team was inspired by the seven skills of highly effective", "#", "Григорий", "static/img/photo-elon-musk.jpg",
+		"#", 97, 1010,
+	},
+	{"end", "static/img/endOfFeed.png", []string{"IT-News", "Study"},
+		"А всё, а раньше надо было", "", "#", "Tester-ender",
+		"static/img/loader-1-HorizontalBalls.gif", "#", 0, 0,
 	},
 }
 
@@ -355,9 +425,9 @@ func Run() {
 	api := NewMyHandler()
 
 	e.POST("/login", api.Login)
-	e.POST("api/v1/user/signup", api.Register)
+	e.POST("/signup", api.Register)
 	e.POST("api/v1/user/logout", api.Logout)
-	e.POST("api/v1/user/getfeed", api.Getfeed)
+	e.GET("/feed", api.Getfeed)
 	e.GET("/", api.Root)
 
 	e.Logger.Fatal(e.Start("192.168.0.104:8081"))
