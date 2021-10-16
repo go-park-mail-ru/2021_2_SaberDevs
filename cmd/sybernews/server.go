302fbe3a3d62d9a4ed3c433853a86eb73a231ab5
Yura <yura11011@ya.ru>
2021-10-16 23:49:31 +0300

    interfaces working&

--- cmd/sybernews/server.go
+++ cmd/sybernews/server.go
@@ -4,22 +4,24 @@ import (
 	"net/http"
 
 	ahandler "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/article/delivery"
+	usecase "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/article/usecase"
 	uhandler "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/user/handler"
 
 	"github.com/labstack/echo/v4/middleware"
 
-	"github.com/labstack/echo/v4"
+	echo "github.com/labstack/echo/v4"
 )
 
 func router(e *echo.Echo) {
 	userApi := uhandler.NewUserHandler()
-	articlesApi := ahandler.NewArticlesHandler()
+	us := usecase.NewArticleUsecase()
+	articlesApi := ahandler.NewArticlesHandler(e, us)
+	e.GET("/feed", articlesApi.GetFeed)
 
 	e.POST("/login", userApi.Login)
 	e.POST("/signup", userApi.Register)
 	e.POST("/logout", userApi.Logout)
 
-	e.GET("/feed", articlesApi.GetFeed)
 }
 
 func Run(address string) {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/go-park-mail-ru/2021_2_SaberDevs
 go 1.13
 
 require (
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.6.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.9.0
