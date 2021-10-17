f7146b6ae35bc392d50a4650d43e55ea2418ea90
Yura <yura11011@ya.ru>
2021-10-17 18:39:11 +0300

    cors working 127.0.0.1

--- cmd/sybernews/server.go
+++ cmd/sybernews/server.go
@@ -27,7 +27,7 @@ func router(e *echo.Echo) {
 func Run(address string) {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"http://localhost:8080"},
+		AllowOrigins:     []string{"http://127.0.0.1:8080"},
 		AllowMethods:     []string{http.MethodGet, http.MethodPost},
 		AllowCredentials: true,
 	}))
