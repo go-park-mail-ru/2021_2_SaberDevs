77d58f4b940abd41ae14caf3d10f9c430e954ccb
Yura <yura11011@ya.ru>
2021-10-17 18:59:08 +0300

    middleware

--- cmd/sybernews/server.go
+++ cmd/sybernews/server.go
@@ -8,6 +8,7 @@ import (
 	uhandler "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/user/handler"
 
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 
 	echo "github.com/labstack/echo/v4"
 )
@@ -31,6 +32,15 @@ func Run(address string) {
 		AllowMethods:     []string{http.MethodGet, http.MethodPost},
 		AllowCredentials: true,
 	}))
+
+	e.Use(middleware.RecoverWithConfig(middleware.RecoverConfig{
+		StackSize: 1 << 10, // 1 KB
+		LogLevel:  log.ERROR,
+	}))
+
+	// e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+	// 	TokenLookup: "header:X-XSRF-TOKEN",
+	// }))
 
 	router(e)
 
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.6.1
+	github.com/labstack/gommon v0.3.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.9.0
 	github.com/stretchr/testify v1.7.0
