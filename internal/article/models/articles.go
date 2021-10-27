1e48d38f4a110f9cb92b53d95b08b1d260294f05
Yura <yura11011@ya.ru>
2021-10-27 13:21:25 +0300

    starting new fetch

--- internal/article/models/articles.go
+++ internal/article/models/articles.go
@@ -17,6 +17,20 @@ type Article struct {
 	Likes        uint     `json:"likes"`
 }
 
+type DbArticle struct {
+	Id           int
+	StringId     string `json:"id"`
+	PreviewUrl   string `json:"previewUrl"`
+	Title        string `json:"title"`
+	Text         string `json:"text"`
+	AuthorUrl    string `json:"authorUrl"`
+	AuthorName   string `json:"authorName"`
+	AuthorAvatar string `json:"authorAvatar"`
+	CommentsUrl  string `json:"commentsUrl"`
+	Comments     uint   `json:"comments"`
+	Likes        uint   `json:"likes"`
+}
+
 //Тело ответа на API-call /getfeed
 
 // type RequestChunk struct {
--- internal/article/repository/postgresdb.go
+++ internal/article/repository/postgresdb.go
@@ -3,9 +3,8 @@
 package article
 
 import (
-	"log"
-
 	"context"
+	"fmt"
 
 	amodels "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/article/models"
 	"github.com/jmoiron/sqlx"
@@ -16,16 +15,48 @@ type psqlArticleRepository struct {
 	Db *sqlx.DB
 }
 
+const previewLen = 50
+
+func articleConv(val amodels.DbArticle, Db *sqlx.DB) amodels.Article {
+	var article amodels.Article
+	article.AuthorAvatar = val.AuthorAvatar
+	article.AuthorName = val.AuthorName
+	article.AuthorUrl = val.AuthorUrl
+	article.Comments = val.Comments
+	article.CommentsUrl = val.CommentsUrl
+	article.Id = val.StringId
+	article.Likes = val.Likes
+	article.PreviewUrl = val.PreviewUrl
+	if len(val.Text) <= previewLen {
+		article.Text = val.Text
+	} else {
+		article.Text = val.Text[1:50]
+	}
+	article.Title = val.Title
+	rows, err := Db.Queryx("SELECT * FROM TAGS")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	for rows.Next() {
+		//	err = rows.StructScan(&newArticle)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	return article
+}
+
 func NewpsqlArticleRepository() amodels.ArticleRepository {
 	connStr := "user=postgres dbname=postgres password=yura11011 host=localhost sslmode=disable"
 	db, err := sqlx.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err.Error())
 	}
 
 	err = db.Ping()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err.Error())
 	}
 	//TODO defer db.Close()
 	return &psqlArticleRepository{db}
@@ -34,17 +65,20 @@ func NewpsqlArticleRepository() amodels.ArticleRepository {
 func (m *psqlArticleRepository) Fetch(ctx context.Context, from, chunkSize int) (result []amodels.Article, err error) {
 
 	var ChunkData []amodels.Article
-	rows, err := m.Db.Queryx("SELECT * FROM ARTICLES LIMIT $1 OFFSET $2", from, from+chunkSize)
+	//rows, err := m.Db.Queryx("SELECT * FROM ARTICLES" LIMIT $1 OFFSET $2", from, from+chunkSize")
+	rows, err := m.Db.Queryx("SELECT * FROM ARTICLES")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err.Error())
 	}
-	var newArticle amodels.Article
+	var newArticle amodels.DbArticle
+	var outArticle amodels.Article
 	for rows.Next() {
 		err = rows.StructScan(&newArticle)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err.Error())
 		}
-		ChunkData = append(ChunkData, newArticle)
+		outArticle = articleConv(newArticle, m.Db)
+		ChunkData = append(ChunkData, outArticle)
 		//fmt.Println(newArticle.Id, newArticle.PreviewUrl)
 	}
 
