f16c441c7e7a593851a6bb2470c5728640c4ed84
Yura <yura11011@ya.ru>
2021-10-30 18:34:01 +0300

    basic sanitizer for articles

--- internal/article/usecase/articleUseCase.go
+++ internal/article/usecase/articleUseCase.go
@@ -5,6 +5,8 @@ import (
 
 	"context"
 
+	"github.com/microcosm-cc/bluemonday"
+
 	repository "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/article/repository"
 	"github.com/jmoiron/sqlx"
 
@@ -13,6 +15,25 @@ import (
 
 type articleUseCase struct {
 	articleRepo amodels.ArticleRepository
+}
+
+func SanitizeArticle(a *amodels.Article) *amodels.Article {
+	s := bluemonday.StrictPolicy()
+	l := bluemonday.UGCPolicy()
+	a.AuthorAvatar = s.Sanitize(a.AuthorAvatar)
+	a.AuthorName = s.Sanitize(a.AuthorName)
+	a.AuthorUrl = s.Sanitize(a.AuthorUrl)
+	//a.Comments = s.Sanitize(a.Comments) //not a string
+	a.CommentsUrl = s.Sanitize(a.CommentsUrl)
+	a.Id = s.Sanitize(a.Id)
+	// a.Likes = s.Sanitize(a.Likes)//not a string
+	a.PreviewUrl = s.Sanitize(a.PreviewUrl)
+	for i := range a.Tags {
+		a.Tags[i] = s.Sanitize(a.Tags[i])
+	}
+	a.Text = l.Sanitize(a.Text)
+	a.Title = s.Sanitize(a.Title)
+	return a
 }
 
 func NewArticleUsecase(db *sqlx.DB) amodels.ArticleUseCase {
@@ -45,12 +66,14 @@ func (m *articleUseCase) GetByTag(ctx context.Context, tag string) (result []amo
 	result, err = m.articleRepo.GetByTag(ctx, tag)
 	return result, err
 }
+
 func (m *articleUseCase) GetByAuthor(ctx context.Context, author string) (result []amodels.Article, err error) {
 	result, err = m.articleRepo.GetByTag(ctx, author)
 	return result, err
 }
 
 func (m *articleUseCase) Store(ctx context.Context, a *amodels.Article) error {
+	a = SanitizeArticle(a)
 	err := m.articleRepo.Store(ctx, a)
 	return err
 }
