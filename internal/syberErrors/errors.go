59e7a22ffa269c8470671a4f41cb65cb0027143c
Mollen <necr94@gmail.com>
2021-10-31 00:58:39 +0300

    store done

--- internal/syberErrors/errors.go
+++ internal/syberErrors/errors.go
@@ -120,3 +120,14 @@ type ErrNoSession struct {
 func (e ErrNoSession) Error() string {
 	return fmt.Sprintf("error happend in %s, Reason: %s", e.Function, e.Reason)
 }
+
+// -------------------------------------------------------------------------------
+
+type ErrInternal struct {
+	Reason   string
+	Function string
+}
+
+func (e ErrInternal) Error() string {
+	return fmt.Sprintf("error happend in %s, Reason: %s", e.Function, e.Reason)
+}
--- internal/user/repisitory/userRepository.go
+++ internal/user/repisitory/userRepository.go
@@ -20,24 +20,43 @@ func NewUserRepository(db *sqlx.DB) umodels.UserRepository {
 // TODO error handling
 func (r *userPsqlRepo) GetByEmail(ctx context.Context, email string) (umodels.User, error) {
 	user := umodels.User{}
-	err := r.Db.Get(&user, "SELECT Id, Name, Surname, Email, Password, Score FROM author WHERE Email = $1", email)
+
+	err := r.Db.Get(&user, "SELECT Name, Surname, Email, Password, Score FROM author WHERE Email = $1", email)
 	if err != nil {
 		return user, sbErr.ErrUserDoesntExist{
 			Reason: err.Error(),
 			Function: "userRepositiry/GetByEmail",
 		}
 	}
-	// u, ok := r.users.Load(email)
-	// if !ok {
-	// 	return umodels.User{}, sbErr.ErrUserDoesntExist{
-	// 		Reason:   "no user in memory repo",
-	// 		Function: "userRepository/GetByEmail",
-	// 	}
-	// }
+
 	return user, nil
 }
 
 func (r *userPsqlRepo) Store(ctx context.Context, user *umodels.User) (umodels.User, error) {
+	var email string
+
+	err := r.Db.Get(&email, "SELECT Email FROM author WHERE Email = $1", user.Email)
+	if err != nil {
+		return *user, sbErr.ErrInternal{
+			Reason:  err.Error(),
+			Function: "userRepository/Store",
+		}
+	}
+	if email != "" {
+		return *user, sbErr.ErrUserExists{
+			Reason:  "email already in use",
+			Function: "userRepository/Store",
+		}
+	}
+
+	schema := `INSERT INTO authors (Name, Surname, Email, Password, Score) VALUES ($1, $2, $3, $4, $5)`
+	_, err = r.Db.Exec(schema, user.Name, user.Surname, user.Email, user.Password, 0)
+	if err != nil {
+		return *user, sbErr.ErrInternal{
+			Reason:  err.Error(),
+			Function: "userRepository/Store",
+		}
+	}
 	// r.users.Store(user.Email, user)
 	return *user, nil
 }
