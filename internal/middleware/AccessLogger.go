726a5846d630c3c61bf61c813a0b4021a6ed8b28
Yura <yura11011@ya.ru>
2021-11-02 01:10:39 +0300

    log func

--- internal/middleware/AccessLogger.go
+++ internal/middleware/AccessLogger.go
@@ -1,17 +1,22 @@
 package middleware
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
+func Log(r *http.Request, c echo.Context, start time.Time) {
+	Id := r.Header.Get(echo.HeaderXRequestID)
+	c.Logger().Info("Id = ", Id, " method = ", r.Method, " address = ", r.RemoteAddr, " RequestUri = ", r.RequestURI, " Request Time = ", time.Since(start))
+}
+
 func AccessLogger(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		start := time.Now()
 		err := next(c)
-		Id := c.Request().Header.Get(echo.HeaderXRequestID)
-		c.Logger().Info("Id = ", Id, " method = ", c.Request().Method, " address = ", c.Request().RemoteAddr, " RequestUri = ", c.Request().RequestURI, " Request Time = ", time.Since(start))
+		Log(c.Request(), c, start)
 		return err
 	}
 }
