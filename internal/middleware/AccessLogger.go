4181d02cfa2f77ff1117c02d6173bf98bdeb0998
Yura <yura11011@ya.ru>
2021-11-02 01:11:22 +0300

    naming

--- internal/middleware/AccessLogger.go
+++ internal/middleware/AccessLogger.go
@@ -7,7 +7,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func Log(r *http.Request, c echo.Context, start time.Time) {
+func String(r *http.Request, c echo.Context, start time.Time) {
 	Id := r.Header.Get(echo.HeaderXRequestID)
 	c.Logger().Info("Id = ", Id, " method = ", r.Method, " address = ", r.RemoteAddr, " RequestUri = ", r.RequestURI, " Request Time = ", time.Since(start))
 }
@@ -16,7 +16,7 @@ func AccessLogger(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		start := time.Now()
 		err := next(c)
-		Log(c.Request(), c, start)
+		String(c.Request(), c, start)
 		return err
 	}
 }
