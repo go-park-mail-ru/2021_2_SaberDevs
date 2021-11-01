bc28cf6c957fbb3704a2259b70c3fd19ce755a9e
Yura <yura11011@ya.ru>
2021-11-02 00:43:29 +0300

    hardcoded

--- internal/middleware/AccessLogger.go
+++ internal/middleware/AccessLogger.go
@@ -10,9 +10,6 @@ func AccessLogger(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		start := time.Now()
 		err := next(c)
-		// if err != nil {
-		// 	c.Logger().Error(err)
-		// }
 		Id := c.Request().Header.Get(echo.HeaderXRequestID)
 		c.Logger().Info("Id = ", Id, " method = ", c.Request().Method, " address = ", c.Request().RemoteAddr, " RequestUri = ", c.Request().RequestURI, " Request Time = ", time.Since(start))
 		return err
