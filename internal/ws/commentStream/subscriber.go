db27b4a63263254cb4023128e325b8192f025451
Mollen <necr94@gmail.com>
2021-11-25 05:57:37 +0300

    string splitting and joining

--- internal/ws/commentStream/subscriber.go
+++ internal/ws/commentStream/subscriber.go
@@ -4,6 +4,7 @@ import (
 	cmodels "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/comment/models"
 	"github.com/gorilla/websocket"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -13,6 +14,15 @@ const (
 	pingPeriod     = (pongWait * 8) / 10
 	maxMessageSize = 512
 )
+
+type streamComment struct {
+	Type string `json:"type"`
+	Id          int64  `json:"Id"  db:"id"`
+	Text        string `json:"text" db:"text"`
+	ArticleId   int64  `json:"articleId" db:"articleid"`
+	ArticleName string `json:"articleName" db:"title"`
+	author      `json:"author"`
+}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -34,21 +44,13 @@ func (sub *Subscriber) readWS() {
 		sub.pub.unregister <- sub
 		sub.conn.Close()
 	}()
-	// var c []Comment
-	// c = append(c, Comment{
-	// 	Type:        "stream-comment",
-	// 	Id:          0,
-	// 	Text:        "",
-	// 	ArticleId:   0,
-	// 	ArticleName: "",
-	// })
-	// sub.pub.broadcast <- c
 
 	sub.conn.SetReadLimit(maxMessageSize)
 	err := sub.conn.SetReadDeadline(time.Now().Add(pongWait))
 	if err != nil {
 		return
 	}
+	
 	sub.conn.SetPongHandler(func(string) error {
 		sub.conn.SetReadDeadline(time.Now().Add(pongWait))
 		return nil
@@ -81,11 +83,31 @@ func (sub *Subscriber) writeWS() {
 				sub.conn.WriteMessage(websocket.CloseMessage, []byte{})
 				return
 			}
-
-			err = sub.conn.WriteJSON(message)
-			if err != nil {
-				return
+			
+			for _, comment := range message {
+				articleNameSlice := strings.Split(comment.ArticleName, "")[:25]
+				err = sub.conn.WriteJSON(streamComment{
+					Type:        "stream-comment",
+					Id:          comment.Id,
+					Text:        comment.Text,
+					ArticleId:   comment.ArticleId,
+					ArticleName: strings.Join(articleNameSlice, ""),
+					author:      author{
+						Login:     comment.Login,
+						Surname:   comment.Surname,
+						Name:      comment.Name,
+						AvatarURL: comment.AvatarURL,
+					},
+				})
+				if err != nil {
+					return
+				}
 			}
+
+			// err = sub.conn.WriteJSON(message)
+			// if err != nil {
+			// 	return
+			// }
 
 		case <-ticker.C:
 			err := sub.conn.SetWriteDeadline(time.Now().Add(writeWait)) // всегда err = nil
