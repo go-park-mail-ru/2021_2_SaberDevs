a983e738b298eb464ee74afb60c83e7f3a509f22
Yura <yura11011@ya.ru>
2021-12-18 15:09:01 +0300

    1 more

--- internal/article/handler/test/arHandler_test.go
+++ internal/article/handler/test/arHandler_test.go
@@ -33,6 +33,33 @@ func TestGetByID(t *testing.T) {
 		app.EXPECT().GetByID(gomock.Any(), gomock.Any()).Return(&new, nil).AnyTimes()
 		err := handler.GetByID(c)
 		assert.NoError(t, err)
+		assert.Equal(t, rec.Result().StatusCode, 200)
+	})
+
+}
+
+func TestGetByAuthor(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	app := mock.NewMockArticleDeliveryClient(ctrl)
+
+	handler := hand.NewArticlesHandler(app)
+
+	t.Run("success", func(t *testing.T) {
+		e := echo.New()
+
+		req := httptest.NewRequest(echo.GET, "http://localhost:8081/api/v1/articles/feed?idLastLoaded=9&login=all", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+		rec := httptest.NewRecorder()
+
+		c := e.NewContext(req, rec)
+		new := apps.Repview{}
+		new.Preview = []*apps.Preview{}
+
+		app.EXPECT().Fetch(gomock.Any(), gomock.Any()).Return(&new, nil).AnyTimes()
+		err := handler.GetFeed(c)
+		assert.NoError(t, err)
 	})
 
 }
@@ -47,16 +74,17 @@ func TestGetFeed(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		e := echo.New()
 
-		req := httptest.NewRequest(echo.GET, "http://localhost:8081/api/v1/articles/feed?idLastLoaded=&login=all", nil)
+		req := httptest.NewRequest(echo.GET, "http://localhost:8081/api/v1/articles/author?idLastLoaded=&login=DenisTest", nil)
 		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 
 		rec := httptest.NewRecorder()
 
 		c := e.NewContext(req, rec)
-		new := apps.FullArticle{}
-		new.Author = &apps.Author{}
-		app.EXPECT().GetByID(gomock.Any(), gomock.Any()).Return(&new, nil).AnyTimes()
-		err := handler.GetByID(c)
+		new := apps.Repview{}
+		new.Preview = []*apps.Preview{}
+
+		app.EXPECT().GetByAuthor(gomock.Any(), gomock.Any()).Return(&new, nil).AnyTimes()
+		err := handler.GetByAuthor(c)
 		assert.NoError(t, err)
 	})
 
