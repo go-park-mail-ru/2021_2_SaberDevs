f71bbe3fb08f5f48e61710da1027c7222bee2286
Yura <yura11011@ya.ru>
2021-12-18 19:42:10 +0300

    articles

--- internal/article/server/test/app_test.go
+++ internal/article/server/test/app_test.go
@@ -89,3 +89,54 @@ func TestFetch(t *testing.T) {
 
 	})
 }
+
+func TestFindArticles(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockUcase := mocks.NewMockArticleUsecase(ctrl)
+	mockArticle := models.Preview{
+		Id:          "1",
+		PreviewUrl:  "#",
+		Tags:        []string{"fishing", "boat"},
+		Title:       "fishing",
+		Text:        "fishing is bad for business",
+		Author:      models.Author{Id: 1},
+		CommentsUrl: "#",
+		Comments:    11,
+		Likes:       12,
+	}
+
+	preArticle := []models.Preview{mockArticle}
+	resArticle := &app.Preview{
+		Id:          "1",
+		PreviewUrl:  "#",
+		Tags:        []string{"fishing", "boat"},
+		Title:       "fishing",
+		Text:        "fishing is bad for business",
+		Author:      &app.Author{Id: 1},
+		CommentsUrl: "#",
+		Comments:    11,
+		Likes:       12,
+	}
+	login := "Iam"
+	ch := &app.Chunk{
+		IdLastLoaded: "999999",
+		ChunkSize:    5,
+		Value:        login,
+	}
+	q := &app.Queries{
+		Query: "query",
+		Chunk: ch,
+	}
+	query := "query"
+	repArticle := &app.Repview{Preview: []*app.Preview{resArticle}}
+
+	u := ser.NewArticleManager(mockUcase)
+	t.Run("success", func(t *testing.T) {
+		mockUcase.EXPECT().FindArticles(gomock.Eq(context.TODO()), login, query, "999999", 5).Return(preArticle, nil).AnyTimes()
+		a, err := u.FindArticles(context.TODO(), q)
+		assert.NoError(t, err)
+		assert.Equal(t, a, repArticle)
+
+	})
+}
