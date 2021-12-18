695c53ee6424586f695266da89da053f6660bdc7
Yura <yura11011@ya.ru>
2021-12-18 17:46:33 +0300

    testing

---
+++ internal/article/server/test/app_test.go
@@ -0,0 +1,43 @@
+package testing
+
+import (
+	"context"
+	"testing"
+
+	app "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/article/article_app"
+	mocks "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/article/models/mock"
+	ser "github.com/go-park-mail-ru/2021_2_SaberDevs/internal/article/server/serve"
+	gomock "github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetByID(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	defer ctrl.Finish()
+
+	mockUcase := mocks.NewMockArticleUsecase(ctrl)
+	mockArticle := &app.FullArticle{
+		Id:          "1",
+		PreviewUrl:  "#",
+		Tags:        []string{"fishing", "boat"},
+		Title:       "fishing",
+		Text:        "fishing is bad for business",
+		Author:      &app.Author{Id: 1},
+		CommentsUrl: "#",
+		Comments:    11,
+		Likes:       12,
+	}
+	u := ser.NewArticleManager(mockUcase)
+	login := "Iam"
+	id := &app.Id{Id: "1", Value: login}
+	t.Run("success", func(t *testing.T) {
+		mockUcase.EXPECT().GetByID(gomock.Eq(context.TODO()), gomock.Eq(login), gomock.Eq(int64(1))).Return(mockArticle, nil).AnyTimes()
+
+		a, err := u.GetByID(context.TODO(), id)
+		assert.NoError(t, err)
+		assert.Equal(t, a, mockArticle)
+
+	})
+
+}
