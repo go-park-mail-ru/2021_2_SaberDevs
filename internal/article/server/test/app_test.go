8719ea2e718da3fa6c4d11e19a5ed3e2f065e23c
Yura <yura11011@ya.ru>
2021-12-18 20:10:46 +0300

    out

--- internal/article/server/test/app_test.go
+++ internal/article/server/test/app_test.go
@@ -191,3 +191,187 @@ func TestFindbyTag(t *testing.T) {
 
 	})
 }
+
+func TestGetbyAuthor(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockUcase := mocks.NewMockArticleUsecase(ctrl)
+	mockArticle := models.Preview{
+		Id:          "1",
+		PreviewUrl:  "#",
+		Tags:        []string{"fishing", "boat"},
+		Title:       "fishing",
+		Text:        "fishing is bad for business",
+		Author:      models.Author{Id: 1},
+		CommentsUrl: "#",
+		Comments:    11,
+		Likes:       12,
+	}
+
+	preArticle := []models.Preview{mockArticle}
+	resArticle := &app.Preview{
+		Id:          "1",
+		PreviewUrl:  "#",
+		Tags:        []string{"fishing", "boat"},
+		Title:       "fishing",
+		Text:        "fishing is bad for business",
+		Author:      &app.Author{Id: 1},
+		CommentsUrl: "#",
+		Comments:    11,
+		Likes:       12,
+	}
+	login := "Iam"
+	ch := &app.Chunk{
+		IdLastLoaded: "999999",
+		ChunkSize:    5,
+		Value:        login,
+	}
+	author := "author"
+	au := &app.Authors{
+		Author: author,
+		Chunk:  ch,
+	}
+	repArticle := &app.Repview{Preview: []*app.Preview{resArticle}}
+
+	u := ser.NewArticleManager(mockUcase)
+	t.Run("success", func(t *testing.T) {
+		mockUcase.EXPECT().GetByAuthor(gomock.Eq(context.TODO()), login, author, "999999", 5).Return(preArticle, nil).AnyTimes()
+		a, err := u.GetByAuthor(context.TODO(), au)
+		assert.NoError(t, err)
+		assert.Equal(t, a, repArticle)
+
+	})
+}
+
+func TestGetbyCategory(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockUcase := mocks.NewMockArticleUsecase(ctrl)
+	mockArticle := models.Preview{
+		Id:          "1",
+		PreviewUrl:  "#",
+		Tags:        []string{"fishing", "boat"},
+		Title:       "fishing",
+		Text:        "fishing is bad for business",
+		Author:      models.Author{Id: 1},
+		CommentsUrl: "#",
+		Comments:    11,
+		Likes:       12,
+	}
+
+	preArticle := []models.Preview{mockArticle}
+	resArticle := &app.Preview{
+		Id:          "1",
+		PreviewUrl:  "#",
+		Tags:        []string{"fishing", "boat"},
+		Title:       "fishing",
+		Text:        "fishing is bad for business",
+		Author:      &app.Author{Id: 1},
+		CommentsUrl: "#",
+		Comments:    11,
+		Likes:       12,
+	}
+	login := "Iam"
+	ch := &app.Chunk{
+		IdLastLoaded: "999999",
+		ChunkSize:    5,
+		Value:        login,
+	}
+	category := "category"
+	cat := &app.Categories{
+		Category: category,
+		Chunk:    ch,
+	}
+	repArticle := &app.Repview{Preview: []*app.Preview{resArticle}}
+
+	u := ser.NewArticleManager(mockUcase)
+	t.Run("success", func(t *testing.T) {
+		mockUcase.EXPECT().GetByCategory(gomock.Eq(context.TODO()), login, category, "999999", 5).Return(preArticle, nil).AnyTimes()
+		a, err := u.GetByCategory(context.TODO(), cat)
+		assert.NoError(t, err)
+		assert.Equal(t, a, repArticle)
+
+	})
+}
+
+func TestGetByTag(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockUcase := mocks.NewMockArticleUsecase(ctrl)
+	mockArticle := models.Preview{
+		Id:          "1",
+		PreviewUrl:  "#",
+		Tags:        []string{"fishing", "boat"},
+		Title:       "fishing",
+		Text:        "fishing is bad for business",
+		Author:      models.Author{Id: 1},
+		CommentsUrl: "#",
+		Comments:    11,
+		Likes:       12,
+	}
+
+	preArticle := []models.Preview{mockArticle}
+	resArticle := &app.Preview{
+		Id:          "1",
+		PreviewUrl:  "#",
+		Tags:        []string{"fishing", "boat"},
+		Title:       "fishing",
+		Text:        "fishing is bad for business",
+		Author:      &app.Author{Id: 1},
+		CommentsUrl: "#",
+		Comments:    11,
+		Likes:       12,
+	}
+	login := "Iam"
+	ch := &app.Chunk{
+		IdLastLoaded: "999999",
+		ChunkSize:    5,
+		Value:        login,
+	}
+	tag := "tag"
+	tags := &app.Tags{
+		Tag:   tag,
+		Chunk: ch,
+	}
+	repArticle := &app.Repview{Preview: []*app.Preview{resArticle}}
+
+	u := ser.NewArticleManager(mockUcase)
+	t.Run("success", func(t *testing.T) {
+		mockUcase.EXPECT().GetByTag(gomock.Eq(context.TODO()), login, tag, "999999", 5).Return(preArticle, nil).AnyTimes()
+		a, err := u.GetByTag(context.TODO(), tags)
+		assert.NoError(t, err)
+		assert.Equal(t, a, repArticle)
+
+	})
+}
+
+func TestFindAuthors(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockUcase := mocks.NewMockArticleUsecase(ctrl)
+
+	authors := []models.Author{models.Author{Id: 12}}
+
+	login := "Iam"
+	ch := &app.Chunk{
+		IdLastLoaded: "999999",
+		ChunkSize:    5,
+		Value:        login,
+	}
+	query := "query"
+	q := &app.Queries{
+		Query: query,
+		Chunk: ch,
+	}
+	Auth := &app.Author{Id: 12}
+	repArticle := &app.AView{Author: []*app.Author{Auth}}
+
+	u := ser.NewArticleManager(mockUcase)
+	t.Run("success", func(t *testing.T) {
+		mockUcase.EXPECT().FindAuthors(gomock.Eq(context.TODO()), query, "999999", 5).Return(authors, nil).AnyTimes()
+		a, err := u.FindAuthors(context.TODO(), q)
+		assert.NoError(t, err)
+		assert.Equal(t, a, repArticle)
+
+	})
+}
