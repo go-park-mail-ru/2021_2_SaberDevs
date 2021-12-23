46a515b0834a1d1c17ebe8a4b924c81761383cdb
yura <yura>
2021-12-23 11:34:23 +0300

    zapY

--- cmd/sybernews/server.go
+++ cmd/sybernews/server.go
@@ -228,11 +228,12 @@ func Run(address string) {
 	}
 	// logger.OptionSetLevel("info")
 	defer logger.Sync()
-	logger.Info("failed to fetch URL",
-		// Structured context as strongly typed Field values.
-		zap.String("url", "url"),
-		zap.Int("attempt", 3),
-		zap.Duration("backoff", time.Second),
+	logger.Info("ID=",
+		zap.String("Id", "url"),
+		zap.String("method", "meth"),
+		zap.String("path", "path"),
+		zap.String("status", "status"),
+		zap.Duration("latency", time.Second),
 	)
 	Default := middleware.CSRFConfig{
 		Skipper:        middleware.DefaultSkipper,
