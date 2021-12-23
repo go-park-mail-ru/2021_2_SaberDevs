7be84bb3a1b7f7d06b9f65e41cb8b35cd3e5d989
yura <yura>
2021-12-23 14:41:54 +0300

    one more

--- cmd/sybernews/test/server_test.go
+++ cmd/sybernews/test/server_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	server "github.com/go-park-mail-ru/2021_2_SaberDevs/cmd/sybernews"
+	wrapper "github.com/go-park-mail-ru/2021_2_SaberDevs/internal"
 	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
@@ -38,5 +39,15 @@ func TestTarantoolConnect(t *testing.T) {
 		db, err := server.TarantoolConnect()
 		assert.NoError(t, err)
 		assert.NotEmpty(t, db)
+	})
+}
+
+func TestNewConfig(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("success", func(t *testing.T) {
+		log := wrapper.NewLogger()
+		assert.NotEmpty(t, log)
 	})
 }
