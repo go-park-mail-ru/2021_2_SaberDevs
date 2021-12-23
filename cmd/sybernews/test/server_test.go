4cbe0381da0f7507522aebdf17d8aac8d7821dcc
yura <yura>
2021-12-23 15:14:37 +0300

    ne more

--- cmd/sybernews/test/server_test.go
+++ cmd/sybernews/test/server_test.go
@@ -1,6 +1,8 @@
 package testing
 
 import (
+	"os"
+	"os/exec"
 	"testing"
 
 	server "github.com/go-park-mail-ru/2021_2_SaberDevs/cmd/sybernews"
@@ -50,4 +52,19 @@ func TestNewConfig(t *testing.T) {
 		log := wrapper.NewLogger()
 		assert.NotEmpty(t, log)
 	})
+}
+
+func TestRun(t *testing.T) {
+	if os.Getenv("BE_CRASHER") == "1" {
+		server.Run("crasher")
+		return
+	}
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestRun")
+	cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
 }
